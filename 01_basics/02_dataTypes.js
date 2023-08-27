4c5b5708659316692dc848b3f0cc3bb43aaa4609
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-08-27 13:57:02 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // trat all js codd as newer version 
+
+// alert(2+3) we are using nodejs not browser
+
+let name="avichal"
+let age=22
+let isLoggesIN=false
+
+// number
+//null= standalone value
+// undefined= 
+// symbol = to find uniqueness\
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+//null is an object
+
+
