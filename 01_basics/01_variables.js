fa272aef904a5f3e7619af27e6fe0c250ab6e9bf
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-08-27 12:51:33 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID=22345
+let accountEmail="hello.@google.com"
+var accountPass="12345"
+accountType="savings"
+let accountState
+
+console.table([accountID,accountEmail,accountPass,accountType,accountState])
+
+
+/*
+Dont try to use var more frequently 
+because of issue in block scope and functional scope 
+*/
