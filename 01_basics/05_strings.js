fabc6d63a02e24690a4187095e555da4fe80d999
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-08-28 10:33:47 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,4 +3,34 @@ const repocount=21
 
 // console.log(name+ repocount + " value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName= new String("avichal-kumar")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('a'));
+
+const newString= gameName.substring(0,4);
+// console.log(newString);
+
+
+const another=gameName.slice(-3,4);
+// console.log(another);
+
+const new1= "   avichal   "
+// console.log(new1);
+// console.log(new1.trim());
+
+
+const url= "https://avical.com/avichal%14kumar"
+// console.log(url.replace('%14','-'))
+
+
+
+console.log(gameName.split('-'));
+
+
