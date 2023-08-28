f97a870c2c85b080f846ac5d38c1b1c6f3eea8e7
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-08-28 07:33:12 +0000

    complete

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,10 +12,19 @@ let negValue=-value
 let str1="hello"
 let str2=" avichal"
 let str3=str1+str2
-console.log(str3);
+// console.log(str3);
 
-console.log("1" +2);
-console.log("1" + 2 + 2);
-console.log(1 +"2");
+// console.log("1" +2);
+// console.log("1" + 2 + 2);
+// console.log(1 +"2");
 
-console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2");
+
+// console.log(+"");
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
