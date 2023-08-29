9ddc6056a958c4abeb30cb4c2c461815533e4727
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-08-29 11:10:16 +0000

    Basics 2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,31 @@
+const marvel_heros=["thor","ironman","spiderman"]
+const dc=["superman","batman","aquaman"]
+
+// marvel_heros.push(dc)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3]);
+
+// const  all_heros= marvel_heros.concat(dc)
+// console.log(all_heros);
+
+// const all_newHeros= [...marvel_heros,...dc]
+// console.log(all_newHeros);
+
+
+// const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const real_another_array= another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+
+// console.log(Array.isArray("AVICHAL"))
+// console.log(Array.from("AVICHAL"))
+// console.log(Array.from({name:"avichal"}))// intresting part
+
+
+let a=33
+let b=32
+let c=34
+console.log(Array.of(a,b,c));
