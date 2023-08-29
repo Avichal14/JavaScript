c0629f616bbfc7a12ef655aedb3cad680ece4611
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-08-29 10:55:48 +0000

    arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,40 @@
+// Arrays
+
+const myArr = [1,2,3,4,5,6]
+
+// array in js is resizeable 
+
+// console.log(myArr[3]);
+
+
+const myHeros = ["Ironman","spiderman"]
+
+const myArr2 = new Array(3,5,6,3,2)
+// console.log(myArr2[3]);
+
+//  array methods
+
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr);
+
+
+// myArr.unshift(0)
+// myArr.shift()
+// console.log(myArr);
+
+console.log(myArr.includes(3));
+console.log(myArr.indexOf(3));
+
+
+
+const newArr= myArr.join()
+
+console.log(newArr);
+console.log(myArr);
+
+
+// slice , splice
+
+console.log("A" , myArr);
+;
