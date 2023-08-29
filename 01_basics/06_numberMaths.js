a7a4829cf2f2c19487843a102b9efe69b8e80eb2
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-08-29 06:09:35 +0000

    Maths

---
+++ 01_basics/06_numberMaths.js
@@ -0,0 +1,42 @@
+const score=400
+const balance = new Number(1100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+
+// const num= 1000000
+// console.log(num.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log((Math.abs(-4)));
+// console.log((Math.round(4.6)));
+// console.log((Math.ceil(4.6)));
+// console.log((Math.floor(4.6)));
+// console.log((Math.min(4,6,2,1,8,4)));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+
+const min=10;
+const max=20;
+
+console.log(Math.floor((Math.random () * (max-min + 1))+ min ));
+
+
+
+
+
+
+
+
+
+
+
+
