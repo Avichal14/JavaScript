7ce2695ee6a915181f7d3a326fe927cba62d778d
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-08-30 17:04:05 +0000

    Obj Part 2

---
+++ 02_basics/04_object.js
@@ -0,0 +1,49 @@
+// Singleton
+
+// const tinderUser= new Object() - singleton object
+const tinderUser= {} // non singleton object
+
+tinderUser.id="123abc"
+tinderUser.name = "avichal"
+tinderUser.isLoggedin = false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email :"some@gmail.com",
+    fullname:{
+        username:{
+            firstname: "avichal",
+            lastname: "Kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.username.firstname);
+
+const obj1= {1:"a", 2:"b"}
+const obj2= {3:"a", 4:"b"}
+
+// const obj3={obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 ={...obj1,...obj2}
+// console.log(obj3);
+
+
+const users= [
+    {
+        id:1,
+        email :"h@gmail.com"
+    },
+]
+
+users[0].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedin'));
