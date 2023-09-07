8fd75e4dba1f33867da46a6136bb910c018ecb2c
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-09-07 17:29:58 +0000

    Hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -6,11 +6,40 @@ if(true){
     let a=10;
     const b=20;
     // var c=30;
-    console.log(a);
+    // console.log(a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c); // this will return value its has largest scope
 // var should be avoided 
+
+
+// Nested scope
+
+function one(){
+    const username = "avichal"
+
+    function two(){
+        const website = "udemy"
+        // console.log(username);
+
+    }
+    // console.log(website);
+    two()
+}
+
+one()
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+addtwo(5)
+const addtwo = function(num){
+    return num +2
+}
