11987b6603691da134322fe0f157c64fe0482af1
Avichal Kumar <105901472+Avichal14@users.noreply.github.com>
2023-09-29 08:31:54 +0000

    Control Flow done

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,18 @@
+//if
+
+// const isUserloggedin=true
+
+// if(2==="2"){
+//     console.log("correct");
+// }
+// else{
+//     console.log("no");
+// }
+
+// < ,>,<=,>=,==,===,!=
+
+
+const balance=2000
+// if(balance > 500) console.log("test");
+
+//&& ||
---
+++ 04_controlFlow/truthy.js
@@ -0,0 +1,18 @@
+const email=[]
+// if(email){
+//     console.log("got email");
+// }
+// else{
+//     console.log("not got");
+// }
+
+
+// falsy=> false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy => true, 1, "0", 'false', " ", [], {}, function(){}, 
+
+if(email.length===0){
+    console.log("empty");
+}
+
+// Nullish coalescing operator(??):
